5630de7a38cdbf2fb480a5bca5176012826104be
hthaianh <huynhthaianhhd@gmail.com>
2020-10-11 21:48:18 +0700

    Updates

---
+++ precache-manifest.28ec16e5b503f8e92fdfc7536ec8572b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37aa6fed7488be068618f2a6e489e44",
+    "url": "/todos-hooks/index.html"
+  },
+  {
+    "revision": "15fb842486f90ff5faf0",
+    "url": "/todos-hooks/static/css/main.0da6dc98.chunk.css"
+  },
+  {
+    "revision": "88c30936b15dcb049ebc",
+    "url": "/todos-hooks/static/js/2.d02afdab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-hooks/static/js/2.d02afdab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15fb842486f90ff5faf0",
+    "url": "/todos-hooks/static/js/main.23c0f957.chunk.js"
+  },
+  {
+    "revision": "ffeeb3ff082b546d5c2e",
+    "url": "/todos-hooks/static/js/runtime-main.5dcd33a9.js"
+  }
+]);
